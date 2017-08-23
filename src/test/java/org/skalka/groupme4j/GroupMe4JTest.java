2252b84945228e4c49f6605401218cc2b7e636c0
nnskalka <nnskalka@skalkabuntu>
2017-08-23 01:16:33 -0500

    Added Testing to project

--- src/test/java/org/skalka/groupme4j/GroupMe4JTest.java
+++ src/test/java/org/skalka/groupme4j/GroupMe4JTest.java
@@ -4,27 +4,40 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.skalka.groupme4j.exception.GroupMeAPIException;
 import org.skalka.groupme4j.response.group.Group;
 
 public class GroupMe4JTest {
 	@Test
-	public void testGroupById() throws GroupMeAPIException {
-		/*GroupMe4J groupme = new GroupMe4J("");
-		Assert.assertEquals(groupme.getGroupById("30998975").getMetadata().getCode(), 200);*/
+	public void testGroups() throws Exception {
+		GroupMe4J groupme = new GroupMe4J(retrieveToken());
+		List<Group> groups = groupme.getGroups(1, 10);
+		
+		Assert.assertNotNull(groups);
+		Assert.assertNotEquals(groups.size(), 0);
 	}
 	
 	@Test
-	public void testCreateGroup() throws Exception {
+	public void testFormerGroups() throws Exception {
+		GroupMe4J groupme = new GroupMe4J(retrieveToken());
+		List<Group> groups = groupme.getFormerGroups();
+		
+		Assert.assertNotNull(groups);
+		Assert.assertNotEquals(groups.size(), 0);
+	}
+	
+	@Test
+	public void testCreateAndDestroyGroup() throws Exception {
 		GroupMe4J groupme = new GroupMe4J(retrieveToken());
 		Group g_created = groupme.createGroup("TESTER GROUP");
 		boolean deleted = groupme.destoryGroup(g_created.getId());
 		
 		Assert.assertNotNull(g_created);
+		Assert.assertNotNull(g_created.getGroupId());
 		Assert.assertTrue(deleted);
 	}
 	
