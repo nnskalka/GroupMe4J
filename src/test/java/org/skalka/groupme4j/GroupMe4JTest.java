8f085705e879fa36d9e13cae4344f077e1480307
Nathan <Nathan@10.32.23.108>
2017-08-22 20:11:18 -0500

    Modified test file to accept Access Token file

--- src/test/java/org/skalka/groupme4j/GroupMe4JTest.java
+++ src/test/java/org/skalka/groupme4j/GroupMe4JTest.java
@@ -1,10 +1,35 @@
 package org.skalka.groupme4j;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
 import org.junit.Test;
+import org.skalka.groupme4j.exception.GroupMeAPIException;
+import org.skalka.groupme4j.response.group.Group;
 
 public class GroupMe4JTest {
 	@Test
-	public void test() {
-
+	public void testGroupById() throws GroupMeAPIException {
+		/*GroupMe4J groupme = new GroupMe4J("");
+		Assert.assertEquals(groupme.getGroupById("30998975").getMetadata().getCode(), 200);*/
+	}
+	
+	@Test
+	public void testCreateGroup() throws Exception {
+		GroupMe4J groupme = new GroupMe4J(retrieveToken());
+		Group g_created = groupme.createGroup("TESTER GROUP");
+		boolean deleted = groupme.destoryGroup(g_created.getId());
+		
+		Assert.assertNotNull(g_created);
+		Assert.assertTrue(deleted);
+	}
+	
+	private String retrieveToken() throws IOException {
+		Path p = (new File("./access.token")).toPath();
+		return Files.readAllLines(p).stream().map(n -> n.toString()).collect(Collectors.joining(""));
 	}
 }
